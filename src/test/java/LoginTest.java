78b22129ea66785d069145f8dc0fd4c6cd8d6a07
IzkaW <maximax@buziaczek.pl>
2018-01-31 12:29:56 +0100

    branch commit 4

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,16 +1,28 @@
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static junit.framework.TestCase.assertTrue;
 
 public class LoginTest extends ConfigFrontend {
 
     @Test
     public void firstFrontendTest(){
         driver.get(baseUrl);
-        driver.findElement(By.cssSelector(".hp-hero__wrapper .no-widows"));
-        driver.findElement(By.xpath("//*[@id=\"navbar-login-link\"]"));
-        driver.findElement(By.id("wpcom-logo"));
-        driver.findElement(By.className("menu-signup"));
-        driver.findElement(By.cssSelector("div.hp-hero__wrapper")).findElement(By.className("button"));
+
+        WebElement title = driver.findElement(By.cssSelector(".hp-hero__wrapper .no-widows"));
+        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"navbar-login-link\"]"));
+        WebElement logo = driver.findElement(By.id("wpcom-logo"));
+        WebElement getStartedTopBar = driver.findElement(By.className("menu-signup"));
+        WebElement getStartedMain = driver.findElement(By.cssSelector("div.hp-hero__wrapper")).findElement(By.className("is-cta"));
+        // driver.findElement(By.cssSelector("div.hp-hero__wrapper a[title=\"Get Started\"]"));
+
+        assertTrue(title.isDisplayed());
+        assertTrue(loginButton.isDisplayed());
+        assertTrue(logo.isDisplayed());
+        assertTrue(getStartedTopBar.isDisplayed());
+        assertTrue(getStartedMain.isDisplayed());
+
     }
 
 }
